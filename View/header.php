e7b5b7b2b1a3ae78026e12d858dc9005bc27e9e0
ToyToy00 <clement.moulin@viacesi.fr>
2015-09-24 14:59:41 +0200

    Ajout de la base MVC
    Base de header en place (menu basique)

---
+++ View/header.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6 ie-lt10 ie-lt9 ie-lt8 ie-lt7 no-js" ><![endif]-->
+<!--[if IE 7 ]><html class="ie ie7 ie-lt10 ie-lt9 ie-lt8 no-js" ><![endif]-->
+<!--[if IE 8 ]><html class="ie ie8 ie-lt10 ie-lt9 no-js" ><![endif]-->
+<!--[if IE 9 ]><html class="ie ie9 ie-lt10 no-js" ><![endif]-->
+<!--[if gt IE 9]><!--><html class="no-js" ><!--<![endif]-->
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="WelcoM dans le quartier du Panier">
+  <meta name="keywords" content="">
+  <meta name="author" content="WelcoM">
+  <title>WelcoM</title> <!-- website title -->
+  <link type='text/css' href='css/bootstrap.min.css' rel='stylesheet'>
+  <link type='text/css' href='css/style.css' rel='stylesheet'>
+  <link type='text/css' href='css/color.css' rel='stylesheet'>
+  <link rel="icon" href="img/favicon.ico"> <!-- website icon -->
+</head>
+<?php require '/Controller/localization.php'; ?>
+<body>
+  <!--<div class="overlay"></div> <!-- overlay -->
+  <!--<div class="pattern"></div> <!-- pattern -->
+
+  <!-- preloader -->
+  <div id="preloader">
+    <div id="preloader-img"></div>
+  </div>
+  <!-- /preloader -->
+
+    <nav class="nav-menu">
+        <ul id="menu">
+            <li ><a href="#"></a>Accueil</li>
+            <li ><a href="#"></a>Histoire</li>
+            <div class="dropdown">
+                <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                  Decouvrir
+                  <span class="caret"></span>
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                  <li><a href="#">Restauration</a></li>
+                  <li><a href="#">Se Cultiver</a></li>
+                  <li><a href="#">La Nuit</a></li>
+                  <li><a href="#">Shopping</a></li>
+                </ul>
+            </div>
+            <li ><a href="#"></a>Portfolio</li>
+            <li ><a href="#"></a>Contact</li>
+        </ul>
+    </nav>
+</body>
--- index.php
+++ index.php
@@ -1,8 +1,25 @@
 <?php
+    session_start();
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+    $site_path = realpath(dirname(__FILE__));
+    define ('__SITE_PATH', $site_path);
 
+    $site_url = str_replace('C:\wamp\www\\','/',__SITE_PATH);
+    define ('__SITE_URL', $site_url);
+
+    include (__SITE_PATH . '/View/header.php');
+    include(__SITE_PATH . '/model/Security.php');
+    include(__SITE_PATH . '/model/user.php');
+
+    if (!empty($_GET['page']) && is_file('Controller/'.$_GET['page'].'.class.php'))
+    {
+            include 'Controller/'.$_GET['page'].'.php';
+    }
+    else
+    {
+            include 'Controller/index.class.php';
+    }
+
+    include 'View/footer.php';
+
+?>
